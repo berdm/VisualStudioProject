a7a6ae6b8f766f0fbcb3c4a80f2d72e36a0a2076
Buket Erdem <46616338+berdm@users.noreply.github.com>
2022-06-20 20:52:51 +0300

    Add files via upload

---
+++ CoordinateTriangle/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace BG_Odev3
+{
+    partial class frmOdev
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btn2Beksen = new System.Windows.Forms.Button();
+            this.btn3Beksen = new System.Windows.Forms.Button();
+            this.btnİzoizDusumu = new System.Windows.Forms.Button();
+            this.btnOtele = new System.Windows.Forms.Button();
+            this.btnDondur = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btn2Beksen
+            // 
+            this.btn2Beksen.Location = new System.Drawing.Point(31, 45);
+            this.btn2Beksen.Name = "btn2Beksen";
+            this.btn2Beksen.Size = new System.Drawing.Size(110, 38);
+            this.btn2Beksen.TabIndex = 1;
+            this.btn2Beksen.Text = "2B eksen Çiz";
+            this.btn2Beksen.UseVisualStyleBackColor = true;
+            this.btn2Beksen.Click += new System.EventHandler(this.btn2Beksen_Click);
+            // 
+            // btn3Beksen
+            // 
+            this.btn3Beksen.Location = new System.Drawing.Point(31, 89);
+            this.btn3Beksen.Name = "btn3Beksen";
+            this.btn3Beksen.Size = new System.Drawing.Size(110, 39);
+            this.btn3Beksen.TabIndex = 2;
+            this.btn3Beksen.Text = "3B eksen";
+            this.btn3Beksen.UseVisualStyleBackColor = true;
+          
+            // 
+            // btnİzoizDusumu
+            // 
+            this.btnİzoizDusumu.Location = new System.Drawing.Point(31, 134);
+            this.btnİzoizDusumu.Name = "btnİzoizDusumu";
+            this.btnİzoizDusumu.Size = new System.Drawing.Size(110, 46);
+            this.btnİzoizDusumu.TabIndex = 3;
+            this.btnİzoizDusumu.Text = "izomterik İzdüşümü Al";
+            this.btnİzoizDusumu.UseVisualStyleBackColor = true;
+            this.btnİzoizDusumu.Click += new System.EventHandler(this.btnİzoizDusumu_Click);
+            // 
+            // btnOtele
+            // 
+            this.btnOtele.Location = new System.Drawing.Point(31, 186);
+            this.btnOtele.Name = "btnOtele";
+            this.btnOtele.Size = new System.Drawing.Size(110, 45);
+            this.btnOtele.TabIndex = 4;
+            this.btnOtele.Text = "Ötele";
+            this.btnOtele.UseVisualStyleBackColor = true;
+            this.btnOtele.Click += new System.EventHandler(this.btnOtele_Click);
+            // 
+            // btnDondur
+            // 
+            this.btnDondur.Location = new System.Drawing.Point(31, 237);
+            this.btnDondur.Name = "btnDondur";
+            this.btnDondur.Size = new System.Drawing.Size(110, 50);
+            this.btnDondur.TabIndex = 5;
+            this.btnDondur.Text = "Döndür";
+            this.btnDondur.UseVisualStyleBackColor = true;
+            this.btnDondur.Click += new System.EventHandler(this.btnDondur_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(31, 293);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(110, 43);
+            this.btnTemizle.TabIndex = 6;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // frmOdev
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnDondur);
+            this.Controls.Add(this.btnOtele);
+            this.Controls.Add(this.btnİzoizDusumu);
+            this.Controls.Add(this.btn3Beksen);
+            this.Controls.Add(this.btn2Beksen);
+            this.Name = "frmOdev";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ödev";
+            this.Load += new System.EventHandler(this.frmOdev_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btn2Beksen;
+        private System.Windows.Forms.Button btn3Beksen;
+        private System.Windows.Forms.Button btnİzoizDusumu;
+        private System.Windows.Forms.Button btnOtele;
+        private System.Windows.Forms.Button btnDondur;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ CoordinateTriangle/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BG_Odev3
+{
+    public partial class frmOdev : Form
+    {
+
+        System.Drawing.Graphics Cizim;
+
+        Pen kalem = new Pen(Color.Purple, 2);
+
+        Double[,] XCismiNoktasi = new Double[3, 3] {  {2,3,0},
+                                                      {7,3,0},
+                                                      {2,6,0},};
+
+        Double[,] Xnew = new Double[3, 3];
+
+        Double[,] Tisometric = new Double[4, 4] { { 0.707, -0.408,0,0},
+                                                   {0,0.816,0,0 },
+                                                   {-0.707, -0.408,0,0 },
+                                                    {0,0,0,0 } };
+        Double[,] Ttranslation = new Double[4, 4] { {1,0,0,0 },
+                                                  {0,1,0,0 },
+                                                   {0,0,1,0 },
+                                                      {0,0,0,1 }};
+        Double[,] Trotate = new Double[4, 4] { {1,0,0,0 },
+                                                  {0,1,0,0 },
+                                                   {0,0,1,0 },
+                                                      {0,0,0,1 }};
+
+        Double[,] axis2BX = new Double[2, 2] { { -20, 0 }, { 20, 0 } };
+        Double[,] axis2BY = new Double[2, 2] { { 0, 20 }, { 0, -20 } };
+
+        int i, j, k;
+        public frmOdev()
+        {
+            InitializeComponent();
+        }
+
+        //cisim icin
+        private void Draw()
+        {
+            Cizim = this.CreateGraphics();
+            Cizim.DrawLine(kalem, CaCoordinateX(Xnew[0, 0]), CaCoordinateY(Xnew[0, 1]), CaCoordinateX(Xnew[1, 0]), CaCoordinateY(Xnew[1, 1]));
+            Cizim.DrawLine(kalem, CaCoordinateX(Xnew[1, 0]), CaCoordinateY(Xnew[1, 1]), CaCoordinateX(Xnew[2, 0]), CaCoordinateY(Xnew[2, 1]));
+            Cizim.DrawLine(kalem, CaCoordinateX(Xnew[2, 0]), CaCoordinateY(Xnew[2, 1]), CaCoordinateX(Xnew[0, 0]), CaCoordinateY(Xnew[0, 1]));
+        }
+
+        //yansıtılmış cismin cizimi
+        private void Draw2()
+        {
+            Cizim = this.CreateGraphics();
+            Cizim.DrawLine(kalem, YansıtX(Xnew[0, 0]), YansıtY(Xnew[0, 1]), YansıtX(Xnew[1, 0]), YansıtY(Xnew[1, 1]));
+            Cizim.DrawLine(kalem, YansıtX(Xnew[1, 0]), YansıtY(Xnew[1, 1]), YansıtX(Xnew[2, 0]), YansıtY(Xnew[2, 1]));
+            Cizim.DrawLine(kalem, YansıtX(Xnew[2, 0]), YansıtY(Xnew[2, 1]), YansıtX(Xnew[0, 0]), YansıtY(Xnew[0, 1]));
+        }
+
+        //cisim icin
+        private int CaCoordinateX(Double tempX)
+        {
+            return Convert.ToInt32(300 + 250 + (50 * tempX));
+        }
+
+        private int CaCoordinateY(Double tempY)
+        {
+            return Convert.ToInt32(100 + 250 + (-50 * tempY));
+        }
+
+        //yansıtma icin
+        private int YansıtX(Double tempX)
+        {
+            return Convert.ToInt32(300 + 250 + (50 * tempX));
+        }
+        private int YansıtY(Double tempY)
+        {
+            return Convert.ToInt32(100 + 250 + (50 * tempY));
+        }
+
+
+
+
+        private void btn2Beksen_Click(object sender, EventArgs e)
+        {
+            Cizim = this.CreateGraphics();
+            Cizim.DrawLine(kalem, CaCoordinateX(axis2BX[0, 0]), CaCoordinateY(axis2BX[0, 1]), CaCoordinateX(axis2BX[1, 0]), CaCoordinateY(axis2BX[1, 1]));
+            Cizim.DrawLine(kalem, CaCoordinateX(axis2BY[0, 0]), CaCoordinateY(axis2BY[0, 1]), CaCoordinateX(axis2BY[1, 0]), CaCoordinateY(axis2BY[1, 1]));
+
+            Cizim.Dispose();
+        }
+
+        private void btnİzoizDusumu_Click(object sender, EventArgs e)
+        {
+            Xnew = XCismiNoktasi;
+            Draw();
+        }
+
+        private void btnOtele_Click(object sender, EventArgs e)
+        {
+
+
+            Xnew = XCismiNoktasi;
+            Draw2();
+
+
+        }
+
+        private void btnDondur_Click(object sender, EventArgs e)
+        {
+            Double[,] temp = new Double[8, 4];
+            Double teta = 30;
+
+            Trotate[1, 2] = Math.Cos(Math.PI * teta / 180.0);
+            Trotate[2, 2] = Trotate[1, 1];
+
+            Trotate[1, 2] = Math.Sin(Math.PI * teta / 180.0);
+            Trotate[2, 3] = (-1) * Trotate[1, 2];
+
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    temp[i, j] = 0;
+                    for (k = 0; k < 4; k++)
+                    {
+                        temp[i, j] += XCismiNoktasi[i, k] * Trotate[k, j];
+                    }
+                }
+            }
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    Xnew[i, j] = 0;
+                    for (k = 0; k < 4; k++)
+                    {
+                        Xnew[i, j] += temp[i, k] * Tisometric[k, j];
+                    }
+                }
+            }
+            kalem.Color = Color.Black;
+            Draw();
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+        private void frmOdev_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
